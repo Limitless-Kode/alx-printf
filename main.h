df14ec6e5c52dc310795f7562b6e79b6e54790aa
codyngpriest <vilhobanike@gmail.com>
2023-04-22 20:57:21 +0000

    created main.h file

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/* produces the output according to a format */
+int _printf(const char *format, ...);
+
+int _putchar(char c);
+int _strlen(char *str);
+int _putint(int n);
+int _putunsigned(unsigned int n);
+int _printf(const *format, ...);
+
+
+#endif /* MAIN_H */
