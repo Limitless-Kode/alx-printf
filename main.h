4b3fad7b93a45fa8cbd40d5d655671eeb40c5e72
codyngpriest <vilhobanike@gmail.com>
2023-04-24 21:35:32 +0000

    updated main.h

--- main.h
+++ main.h
@@ -10,7 +10,7 @@ int _putchar(char c);
 int _strlen(char *str);
 int _putstring(char *s);
 int _putint(int n);
-int _putunsigned_int(unsigned int n);
+int _putunsigned(unsigned int n);
 int _puthex(unsigned int n, int uppercase);
 int _putpointer(void *p);
 int _putoctal(unsigned int n);
