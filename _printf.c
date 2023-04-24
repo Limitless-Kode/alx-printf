5e722c77e1bb23702a5a97867f298dffdb03d2c9
Peter Claver Amobila <57482239+Limitless-Kode@users.noreply.github.com>
2023-04-24 22:47:17 +0000

    modified printf to be 40 lines

--- _printf.c
+++ _printf.c
@@ -9,9 +9,7 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
-
 va_start(args, format);
-
 while (format && *format)
 {
 if (*format == '%')
@@ -21,14 +19,23 @@ if (*format == '\0')
 break;
 switch (*format)
 {
-case 'c': count += _putchar(va_arg(args, int)); break;
-case 's': count += _putstring(va_arg(args, char *)); break;
-case 'd': case 'i': count += _putint(va_arg(args, int)); break;
-case 'u': count += _putunsigned(va_arg(args, unsigned int)); break;
-case 'x': case 'X': count += _puthex(va_arg(args, unsigned int), *format == 'X' ? 1 : 0) - (*format == 'X' ? 32 : 0); break;
-case 'o': count += _putoctal(va_arg(args, unsigned int)); break;
-case 'p': count += _putpointer(va_arg(args, void *)); break;
-default: count += _putchar('%'); if (*format) count += _putchar(*format); break;
+case 'c': count += _putchar(va_arg(args, int)); 
+            break;
+case 's': count += _putstring(va_arg(args, char *)); 
+            break;
+case 'd': case 'i': count += _putint(va_arg(args, int)); 
+            break;
+case 'u': count += _putunsigned(va_arg(args, unsigned int)); 
+            break;
+case 'x': case 'X': count += _puthex(va_arg(args, unsigned int), *format == 'X' ? 1 : 0) 
+            - (*format == 'X' ? 32 : 0); 
+            break;
+case 'o': count += _putoctal(va_arg(args, unsigned int)); 
+            break;
+case 'p': count += _putpointer(va_arg(args, void *)); 
+            break;
+default: count += _putchar('%'); if (*format) count += _putchar(*format); 
+            break;
 }
 }
 else
